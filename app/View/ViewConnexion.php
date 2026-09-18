608728df9881f5e3c47edd92c526ea2b420a35d5
yanisseuh <yanisse956@gmail.com>
2026-09-18 15:01:12 +0200

    Update ViewConnexion.php

---
+++ app/View/ViewConnexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./public/assets/scripts/main.js" defer></script>
+    <link rel="stylesheet" href="./public/assets/styles/main.css">
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connectez-vous</h1>
+    <form action="" method="post">
+        <label for="email">Veuillez saisir votre email :</label>
+        <input type="email" name="email">
+        <label for="password">Veuillez saisir votre mot de passe:</label>
+        <input type="password" name="password">
+        <input type="submit" name="submit" value="connexion">
+    </form>
+</body>
+</html>
